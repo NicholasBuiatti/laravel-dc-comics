d4ec2dcf5d96c5f6cbfd3b084a3566cee604c7c4
NicholasBuiatti <nic.buiatti@gmail.com>
2024-07-05 18:36:09 +0200

    creazioen pagina create.blade

--- resources/views/comics/create.blade.php
+++ resources/views/comics/create.blade.php
@@ -1,5 +1,36 @@
 @extends('layouts.app')
 
 @section('content')
-	<h1>Questa è il mio create</h1>
+	<section class="container">
+		<h1>Aggiungi un Fumetto</h1>
+		<form action="" method="">
+
+			<div class="mb-3 col-4">
+				<label class="form-label">TITOLO</label>
+				<input type="text" class="form-control" name="title" required>
+			</div>
+			<div class="mb-3">
+				<label class="form-label">DESCRIZIONE</label>
+				<textarea type="text" class="form-control" name="description" required></textarea>
+			</div>
+			<div class="mb-3">
+				<label class="form-label">URL IMMAGINE COPERTINA</label>
+				<input type="text" class="form-control" name="thumb" required>
+			</div>
+			<div class="mb-3 col-2">
+				<label class="form-label">PREZZO</label>
+				<input type="number" min="0.99" max="999.99" class="form-control" name="price" required>
+			</div>
+			<div class="mb-3 col-4">
+				<label class="form-label">SERIE</label>
+				<input type="text" class="form-control" name="series" required>
+			</div>
+			<div class="mb-3 col-2">
+				<label class="form-label">DATA DI VENDITA</label>
+				<input type="date" class="form-control" name="sale_date" required>
+			</div>
+			<button type="submit" class="btn btn-primary">Submit</button>
+		</form>
+
+	</section>
 @endsection
